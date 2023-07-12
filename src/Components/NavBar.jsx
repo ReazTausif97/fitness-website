ffc8319b5da20bf291a17708c5cb293199777be0
ReazTausi97 <reaztausifff@gmail.com>
2023-07-12 20:49:47 +0600

    navbar added

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,17 +1,30 @@
 import { NavLink } from "react-router-dom";
-
 const NavBar = () => {
   return (
     <>
-      <nav className="flex gap-3 px-48 py-10 justify-end">
-        <NavLink to={"/"}>KRON</NavLink>
-        <NavLink to={"/Contacts"}>Contacts</NavLink>
-        <NavLink to={"/AboutUs"}>AboutUs</NavLink>
-        <NavLink to={"/Faq"}>Faq</NavLink>
-        <NavLink to={"/Membership"}>Membership</NavLink>
-        <NavLink to={"/Team"}>Team</NavLink>
-        <NavLink to={"/Blog"}>Blog</NavLink>
-        <NavLink to={"/Shop"}>Shop</NavLink>
+      <nav className="flex gap-2 px-48 py-10  items-center justify-around">
+        <div>
+          <NavLink className="font-extrabold text-2xl" to={"/"}>
+            KRON
+          </NavLink>
+        </div>
+        <div className="flex gap-3">
+          <NavLink to={"/Contacts"}>Contacts</NavLink>
+          <NavLink to={"/AboutUs"}>AboutUs</NavLink>
+          <NavLink to={"/Faq"}>Faq</NavLink>
+          <NavLink to={"/Membership"}>Membership</NavLink>
+          <NavLink to={"/Team"}>Team</NavLink>
+          <NavLink to={"/Blog"}>Blog</NavLink>
+          <NavLink to={"/Shop"}>Shop</NavLink>
+        </div>
+        <div>
+          <i class="fa-solid fa-user"></i>
+          <i class="ml-3 fa-solid fa-cart-shopping"></i>
+        </div>
+        <div>
+          <button className="btn rounded-none">BUY MEMBERSHIP</button>
+          <button className="ml-3  btn rounded-none">BOOK CLASS</button>
+        </div>
       </nav>
     </>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,16 @@
+import bg from "../assets/bg.jpg";
+
 const Home = () => {
   return (
     <>
-      <h2>Homies </h2>;
+      <h2>home</h2>
+
+      <div
+        className="h-[600px] w-screen bg-cover"
+        style={{
+          backgroundImage: `url(${bg})`,
+        }}
+      ></div>
     </>
   );
 };
