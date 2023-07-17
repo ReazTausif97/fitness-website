881b7c2a59fa164b39037e3fa56d24e64b714378
ReazTausi97 <reaztausifff@gmail.com>
2023-07-17 20:23:59 +0600

    packages added

--- src/Components/MembershipComponenets/MembershipPricing.jsx
+++ src/Components/MembershipComponenets/MembershipPricing.jsx
@@ -2,8 +2,55 @@ const MembershipPricing = () => {
   return (
     <section className="bg-[#CED2DA]">
       <div className="max-w-7xl mx-auto">
-        <h2 className="text-3xl font-bold">PRICING</h2>
-        <h2 className="text-6xl font-bold">CHOOSE YOUR MEMBERSHIP</h2>
+        <div>
+          <h2 className="text-3xl font-bold">PRICING</h2>
+          <h2 className="text-6xl font-bold">CHOOSE YOUR MEMBERSHIP</h2>
+        </div>
+        <div>
+          <div>
+            <h2 className="font-bold text-2xl">3 MONTHS MEMBERSHIP</h2>
+            <div className="flex items-center justify-between">
+              <p className="text-lg w-4/5">
+                3 month membership card will give you access to all our awesome
+                amenities, spaces and group classes. There is no time limit, you
+                can come and go whenever you like.
+              </p>
+              <h2 className="font-bold text-[#D70037] text-xl">$299.99</h2>
+            </div>
+          </div>
+          <div>
+            <h2 className="font-bold text-2xl">10 CLASSES</h2>
+            <div className="flex items-center justify-between">
+              <p className="text-lg w-4/5">
+                10 classes card will give you access to any 10 group classes.
+                You can use the card as long as you like, there is no time
+                limit. The card is rechargeable
+              </p>
+              <h2 className="font-bold text-[#D70037] text-xl">$200.99</h2>
+            </div>
+          </div>
+          <div>
+            <h2 className="font-bold text-2xl">SINGLE CLASS DROP-IN</h2>
+            <div className="flex items-center justify-between">
+              <p className="text-lg w-4/5">
+                You can buy this 1 day pass to join any group class you wish.
+                Ther is no time limit, just choose the class you wish to attend.
+              </p>
+              <h2 className="font-bold text-[#D70037] text-xl">$199.99</h2>
+            </div>
+          </div>
+          <div>
+            <h2 className="font-bold text-2xl">12 MONTHS MEMBERSHIP</h2>
+            <div className="flex items-center justify-between">
+              <p className="text-lg w-4/5">
+                12 month membership card will give you access to all our awesome
+                amenities, spaces and group classes. There is no time limit, you
+                can come and go whenever you like.
+              </p>
+              <h2 className="font-bold text-[#D70037] text-xl">$99.99</h2>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
