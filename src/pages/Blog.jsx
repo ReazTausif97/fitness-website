2d1fb26adf2a922efb0018d5f03791bbc35bff07
ReazTausif97 <reaztausifff@gmail.com>
2023-07-17 08:06:08 +0600

    blog page error solved

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,6 +1,6 @@
 import BlogContainer from "../Components/BlogComponents/BlogContainer";
 import BlogHero from "../Components/BlogComponents/BlogHero";
-import HeaderDetails from "../Components/BlogComponents/headerDetails";
+import HeaderDetails from "../Components/BlogComponents/HeaderDetails";
 import SocialMedia from "../Components/HomeComponents/SocialMedia";
 
 const Blog = () => {
