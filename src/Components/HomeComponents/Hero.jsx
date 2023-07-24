ff7ee23a053ae706ded887c0ad6665b2b764e625
ReazTausif97 <reaztausifff@gmail.com>
2023-07-24 15:01:35 +0600

    home page header responsive issue fixed

--- src/Components/HomeComponents/Hero.jsx
+++ src/Components/HomeComponents/Hero.jsx
@@ -3,15 +3,17 @@ import BtnPrimary from "../Reuse/BtnPrimary";
 const Hero = () => {
   return (
     <section
-      className="bg-[length:100%_100%] lg:bg-cover lg:bg-center h-[700px]"
+      className="bg-cover bg-right h-[700px] sm:bg-cover sm:bg-right lg:bg-cover lg:bg-center "
       style={{
-        backgroundImage: `url("https://i.ibb.co/SdwsMVC/banner.png")`,
+        backgroundImage: `linear-gradient(rgba(255, 255, 255, 0.4), rgba(255, 255, 255, 0.4)),url("https://i.ibb.co/SdwsMVC/banner.png")`,
       }}
     >
       <div className="flex items-center h-full lg:px-0 px-2 md:px-10 lg:max-w-7xl lg:mx-auto">
-        <div className="text-7xl md:text-8xl lg:text-8xl font-extrabold z-10">
-          <h2>KRON</h2>
-          <h2 className="mt-5">FITNESS</h2>
+        <div className="w-full flex flex-col gap-10  lg:text-start md:text-start lg:items-start md:items-start items-center text-center text-5xl md:text-8xl lg:text-8xl font-extrabold z-10">
+          <div>
+            <h2>KRON</h2>
+            <h2 className="">FITNESS</h2>
+          </div>
           <BtnPrimary>Get Free Class</BtnPrimary>
         </div>
       </div>
